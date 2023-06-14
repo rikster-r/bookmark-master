de781a4049d1fec2f633e3e0099444ef39f1390e
rikster-r <seitdanial43@gmail.com>
2023-06-14 22:32:31 +0600

    Fix blue blob overflow

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,31 +5,34 @@ import '../styles/hero.css';
 
 const HeroSection = () => {
   return (
-    <section id="hero" className="section-hero">
-      <div className="img-wrapper img-hero-wrapper">
-        <img src={HeroImage} alt="" className="img-hero" />
-        <div className="bg-blue"></div>
-      </div>
-      <div className="info hero-info">
-        <h1 className="title hero-title">A Simple Bookmark Manager</h1>
-        <p className="subtitle hero-subtitle">
-          A clean and simple interface to organize your favourite websites. Open
-          a new browser tab and see your sites load instantly. Try it for free.
-        </p>
-        <div className="btn-container">
-          <PrimaryButton
-            text="Get it on Chrome"
-            uppercase={false}
-            color="blue"
-          />
-          <PrimaryButton
-            text="Get it on Firefox"
-            uppercase={false}
-            color="gray"
-          />
+    <div className="hero-wrapper">
+      <section id="hero" className="section-hero">
+        <div className="img-wrapper img-hero-wrapper">
+          <img src={HeroImage} alt="" className="img-hero" />
+          <div className="bg-blue"></div>
         </div>
-      </div>
-    </section>
+        <div className="info hero-info">
+          <h1 className="title hero-title">A Simple Bookmark Manager</h1>
+          <p className="subtitle hero-subtitle">
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </p>
+          <div className="btn-container">
+            <PrimaryButton
+              text="Get it on Chrome"
+              uppercase={false}
+              color="blue"
+            />
+            <PrimaryButton
+              text="Get it on Firefox"
+              uppercase={false}
+              color="gray"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
